eb7fd31a082d107dcc13909b86afb09d700aced7
MParry <meilir@live.co.uk>
2025-03-22 14:46:46 +0000

    fix equivalence check

--- automata-explorer/main.cpp
+++ automata-explorer/main.cpp
@@ -215,11 +215,14 @@ public:
 		// format: state -> {symbol -> target}
 		if (is_deterministic) {
 			for (int state : states) {
-				std::cout << state << ": ";
+				// std::cout << state << ": ";
+				printf("%i: \n", state);
 				for (auto t : dfa_transitions[state]) {
-					std::cout << t.first << " -> " << t.second << ", ";
+					// std::cout << t.first << " -> " << t.second << ", ";
+					printf("%c -> %i, \n", t.first, t.second);
 				}
-				std::cout << std::endl;
+				// std::cout << std::endl;
+				printf("-----------------\n");
 			}
 		} else {
 			for (int state : states) {
@@ -506,6 +509,10 @@ public:
 		std::queue<int> queue;
 		std::set<int> visited1;
 
+		state_mapping[dfa1.start] = dfa2.start;
+		queue.push(dfa1.start);
+		visited1.insert(dfa1.start);
+
 		while (!queue.empty()) {
 			int state1 = queue.front();
 			queue.pop();
@@ -548,14 +555,7 @@ public:
 			}
 		}
 
-		// Check accept states
-		for (int state : dfa1.accept) {
-			if (std::find(dfa2.accept.begin(), dfa2.accept.end(), state_mapping[state]) == dfa2.accept.end()) {
-				return false;
-			}
-		}
-
-		return true;
+		return visited1.size() == dfa1.states.size();
 	}
 
 };
@@ -885,7 +885,34 @@ void test_is_equivalent() {
 
 int main(int argc, char *argv[])
 {
-	test_is_equivalent();
+	std::string regex = "a*ba";
+	RegularExpression re(regex);
+	Automaton rdfa = re.get_dfa().minimize_dfa();
+	rdfa.print_transitions();
+
+	// create dfa
+	std::vector<int> states = {0, 1, 2};
+	std::vector<char> alphabet = {'a', 'b'};
+	int start = 0;
+	std::vector<int> accept = {2};
+
+	Automaton dfa(states, alphabet, start, accept, true);
+
+	dfa.add_dfa_transition(0, 'a', 0);
+	dfa.add_dfa_transition(0, 'b', 1);
+	dfa.add_dfa_transition(1, 'a', 2);
+
+	// test dfa
+	std::string input = "aaaaba";
+	std::cout << "DFA on input string: " << input << ". Result: ";
+	std::cout << dfa.accepts(input) << std::endl;
+	Automaton dfa2 = dfa.minimize_dfa();
+	dfa2.print_transitions();
+
+	// check equiv
+	std::cout << "Regex is equivalent to DFA: " << std::endl;
+	std::cout << re.get_dfa().is_equivalent(dfa) << std::endl;
+
 	return 0;
 }
 
@@ -937,6 +964,30 @@ extern "C" {
 
 		std::string input_str(input);
 		return nfa.accepts(input_str) ? 1 : 0;
+	}
+
+	EMSCRIPTEN_KEEPALIVE
+	int regex_vs_dfa(
+		const char* regex, int* states, int states_len, char* alphabet, int alphabet_len,
+		int start, int* accept, int accept_len, int* trans_from, char* trans_symbol, int* trans_to,
+		int trans_len) {
+		// check if dfa is equivalent to regex
+		std::string regex_str(regex);
+		RegularExpression re(regex_str);
+
+		std::vector<int> states_vec(states, states + states_len);
+		std::vector<char> alphabet_vec(alphabet, alphabet + alphabet_len);
+		std::vector<int> accept_vec(accept, accept + accept_len);
+
+		Automaton dfa(states_vec, alphabet_vec, start, accept_vec, true);
+
+		for (int i = 0; i < trans_len; i++ ) {
+			dfa.add_dfa_transition(trans_from[i], trans_symbol[i], trans_to[i]);
+		}
+
+		dfa.print_transitions();
+
+		return re.get_dfa().is_equivalent(dfa);
 	}
 
 	EMSCRIPTEN_KEEPALIVE
